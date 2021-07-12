5fe92c5f564ebcd1d2b0157b2f065bb995522264
amleaa <amalianurain5@gmail.com>
2021-07-12 15:03:15 +0700

    add app.py

--- Web/app.py
+++ Web/app.py
@@ -5,12 +5,16 @@
 # ----------------------------------
 
 # save this as app.py
+
 from flask import Flask, escape, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def satu():
-    # Nah ini menu utamanya
-    return "Menu Utama"
+    # Nah ini menu utamany
+    a = 1+1
+    return "abd {}".format(a)
 
+if  __name__ =="__main__":
+    app.run()
